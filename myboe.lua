bb2b63e7f702c96bd90bdb4b5012f3c1161a1508
Te1amon <72522683+nuldylan@users.noreply.github.com>
2021-07-02 12:09:39 -0400

    Create myboe.lua

---
+++ myboe.lua
@@ -0,0 +1,10 @@
+_G.PurpleWaitTime = 0.3
+game:GetService("ReplicatedStorage").ChrModels.Delinquent:Destroy()
+wait()
+pog = game:GetService("ReplicatedStorage").ChrModels.Myboe:Clone()
+pog.Parent = game:GetService("ReplicatedStorage").ChrModels
+pog.Name = "Delinquent"
+wait()
+game.Players.LocalPlayer.Data.Skin.Value = "Myboe"
+wait()
+pog = game:GetService("ReplicatedStorage").Events.LoadCharacter:FireServer()
