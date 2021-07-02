b1a64637775994416a231206fbe1508a36bbb967
Te1amon <72522683+nuldylan@users.noreply.github.com>
2021-07-02 12:10:33 -0400

    Create monkydrip.lua

---
+++ monkydrip.lua
@@ -0,0 +1,10 @@
+_G.PurpleWaitTime = 0.3
+game:GetService("ReplicatedStorage").ChrModels.Delinquent:Destroy()
+wait()
+pog = game:GetService("ReplicatedStorage").ChrModels.["Monky With Drip"]:Clone()
+pog.Parent = game:GetService("ReplicatedStorage").ChrModels
+pog.Name = "Delinquent"
+wait()
+game.Players.LocalPlayer.Data.Skin.Value = "Monky With Drip"
+wait()
+pog = game:GetService("ReplicatedStorage").Events.LoadCharacter:FireServer()
