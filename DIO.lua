5177b50b38e971c8e7ae6122bb279f9fadb93397
Te1amon <72522683+nuldylan@users.noreply.github.com>
2021-07-02 12:08:03 -0400

    Create DIO.lua

---
+++ DIO.lua
@@ -0,0 +1,10 @@
+_G.PurpleWaitTime = 0.3
+game:GetService("ReplicatedStorage").ChrModels.Delinquent:Destroy()
+wait()
+pog = game:GetService("ReplicatedStorage").ChrModels.DIO:Clone()
+pog.Parent = game:GetService("ReplicatedStorage").ChrModels
+pog.Name = "Delinquent"
+wait()
+game.Players.LocalPlayer.Data.Skin.Value = "DIO"
+wait()
+pog = game:GetService("ReplicatedStorage").Events.LoadCharacter:FireServer()
