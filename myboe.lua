4fe69d147f1de574f5be8cda6efa2552ec72345b
Te1amon <72522683+nuldylan@users.noreply.github.com>
2021-07-03 19:23:13 -0400

    Update myboe.lua

--- myboe.lua
+++ myboe.lua
@@ -7,6 +7,8 @@ pog.Name = "Delinquent"
 wait()
 game.Players.LocalPlayer.Data.Skin.Value = "Myboe"
 wait()
+game:GetService("ReplicatedStorage").ItemData.Quality.Default.Value = game:GetService("ReplicatedStorage").ItemData.Quality.Admin.Value
+wait()
 game:GetService("ReplicatedStorage").FavoriteWeapons.Delinquent.Value = "Darkheart"
 wait()
 pog = game:GetService("ReplicatedStorage").Events.LoadCharacter:FireServer()
